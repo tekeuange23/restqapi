9363c82da97191add562302e15d2698a3ba1e5a0
Olivier <rodomond.o@gmail.com>
2020-03-18 22:50:36 +0700

    update config

--- config.js
+++ config.js
@@ -1,12 +1,23 @@
 const fs = require('fs')
+const path = require('path')
 const YAML = require('yaml')
 
-require('dotenv').config();
+function error(msg) {
+  console.error(`-----`)
+  console.error(msg)
+  console.error(`-----`)
+  process.exit(1)
+}
 
 const ENV_PROPERTY = 'API_ENV'
 
-const url = __dirname + '/.restqa.yml'
-const file = fs.readFileSync(url, 'utf8')
+const configFile = path.join(process.env.INIT_CWD, '.restqa.yml')
+
+if (!fs.existsSync(configFile)) {
+  error(`THE RESQA CONFIG FILE IS MISSING (${configFile})`)
+}
+
+const file = fs.readFileSync(configFile, 'utf8')
 
 const config = YAML.parse(file)
 const envs = config.environments.map(env => env.name)
@@ -14,10 +25,7 @@ const envs = config.environments.map(env => env.name)
 config.env = process.env.API_ENV.toLowerCase()
 
 if (!config.env || !envs.includes(config.env)) {
-  console.error(`-----`)
-  console.error(`THE ENV VARIABLE "${ENV_PROPERTY}" NEEDS TO BE DEFINED (${envs.join(' | ')})`)
-  console.error(`-----`)
-  process.exit(1)
+  error(`THE ENV VARIABLE "${ENV_PROPERTY}" NEEDS TO BE DEFINED (${envs.join(' | ')})`)
 }
 
 config.api = config.environments.find(env => config.env === env.name.toLowerCase())

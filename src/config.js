f942dade53485e20253af701e49a7b4a1351a7ab
Olivier <rodomond.o@gmail.com>
2020-04-19 22:48:24 +0700

    oups creds got leaked need to disabled it

--- src/config.js
+++ src/config.js
@@ -1,6 +1,3 @@
-let GOOGLE_SHEET_ID = '13RqudwkMjp9ilOVM9L26r63FKN6-OlIveGc95q3YLsM'
-let GOOGLE_SHEET_APIKEY = 'AIzaSyAlhYZlVSUimjtT1EluwLv5bXWEvobMvbU'
-
 module.exports = {
   name: 'local',
   url: 'http://host.docker.internal:8080',
@@ -10,8 +7,8 @@ module.exports = {
   data: {
     channel: 'google-sheet',
     config: {
-      id: GOOGLE_SHEET_ID,
-      apikey: GOOGLE_SHEET_APIKEY,
+      id: process.env.GOOGLE_SHEET_ID,
+      apikey: process.env.GOOGLE_SHEET_APIKEY,
     },
     startSymbol: '{[',
     endSymbol: ']}'
